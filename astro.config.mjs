4c87a74203709bb35a3f334a0159323ff3796d0e
Jacek Palczewski <jpalczewski@gmail.com>
2023-08-09 11:07:24 +0200

    feat: add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [sitemap(), prefetch()],
   site: "https://jpalczewski.github.io"
 });
