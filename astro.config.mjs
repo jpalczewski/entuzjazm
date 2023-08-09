7656e9047271dbb06fe6723d20ad6c9496b3820f
Jacek Palczewski <jpalczewski@gmail.com>
2023-08-09 17:55:40 +0200

    feat: tags page, initial svelte and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import prefetch from "@astrojs/prefetch";
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), prefetch()],
-  site: "https://jpalczewski.github.io",
+  integrations: [sitemap(), prefetch(), tailwind(), svelte()],
+  site: "https://jpalczewski.github.io"
 });
