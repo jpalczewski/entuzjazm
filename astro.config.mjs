b4d469e923ba8e98b0c1dce4559948f939d711b2
Jacek Palczewski <jpalczewski@gmail.com>
2023-08-09 05:22:39 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap()],
+  site: "https://jpalczewski.github.io"
+});
