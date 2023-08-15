06da92fe8b15fecd19e092c269af56a78e315ce6
Jacek Palczewski <jpalczewski@gmail.com>
2023-08-15 10:04:39 +0200

    feat: add robots.txt
    fix #8

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), prefetch(), tailwind(), svelte()],
+  integrations: [sitemap(), prefetch(), tailwind(), svelte(), robotsTxt()],
   site: "https://jpalczewski.github.io"
 });
