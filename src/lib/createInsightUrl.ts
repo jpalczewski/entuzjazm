e35079f2f538d7af0512abb627b17dd049048f94
Jacek Palczewski <jpalczewski@gmail.com>
2023-08-16 00:58:07 +0200

    feat: migrate urls #11

---
+++ src/lib/createInsightUrl.ts
@@ -0,0 +1,9 @@
+import type { CollectionEntry } from "astro:content";
+import createSlug from "./createSlug";
+
+export default function (insight: CollectionEntry<'insight'>) {
+    const date = new Date(insight.data.date);
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    return (`/${insight.data.language}/insight/${date.getFullYear()}/${month}/${createSlug(insight.data.title)}`);
+      
+  }
