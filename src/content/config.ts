a30fdada47213a891c57c95d35a7397b74835c13
Jacek Palczewski <jpalczewski@gmail.com>
2023-08-19 12:51:16 +0200

    feat(personal): add content type for personal

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,14 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection, reference } from "astro:content";
 
+const languageType = z.enum(["pl", "en"]);
 // 2. Define a `type` and `schema` for each collection
 const insightCollection = defineCollection({
   type: "content", // v2.5.0 and later
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()).optional(),
-    language: z.enum(["pl", "en"]),
+    language: languageType,
     date: z.string().datetime(),
     relatedInsights: z.array(reference("insight")).optional(),
     referencedUrls: z
@@ -16,7 +17,21 @@ const insightCollection = defineCollection({
   }),
 });
 
+const personalCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()).optional(),
+    language: languageType,
+    date: z
+      .string()
+      .datetime()
+      .transform((date) => new Date(date)),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
   insight: insightCollection,
+  personal: personalCollection,
 };
---
+++ src/content/personal/pl/post-1.md
@@ -0,0 +1,7 @@
+---
+title: "test"
+language: pl
+date: "2023-08-08T12:23:43Z"
+---
+
+Test test
