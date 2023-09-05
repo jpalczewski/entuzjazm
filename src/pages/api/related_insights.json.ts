bec97159ebecf33ad8aeaf376d5491007f03990a
Jacek Palczewski <jpalczewski@gmail.com>
2023-09-05 23:50:24 +0200

    fix(project): add url to related insights
    closes  #64

--- src/pages/api/related_insights.json.ts
+++ src/pages/api/related_insights.json.ts
@@ -1,15 +1,26 @@
 import { getCollection } from "astro:content";
+import { createInsightUrl } from "src/lib/createUrls";
+
+interface relatedInsight {
+  title: string;
+  url: string;
+  tags: string[] | undefined;
+}
 
 export async function GET() {
   const insights = await getCollection("insight");
 
-  const relatedProjectsDict: Record<string, string[]> = {};
+  const relatedProjectsDict: Record<string, relatedInsight[]> = {};
 
   insights.forEach((entry) => {
     entry.data.relatedProjects?.forEach((relatedProject) => {
       relatedProjectsDict[relatedProject.id] = [
         ...(relatedProjectsDict[relatedProject.id] || []),
-        entry.id,
+        {
+          title: entry.data.title,
+          url: createInsightUrl(entry),
+          tags: entry.data.tags,
+        },
       ];
     });
   });
