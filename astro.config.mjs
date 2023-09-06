8a3146be129592854d20ad8f9e58aa5b1949aaee
Jacek Palczewski <jpalczewski@gmail.com>
2023-09-06 23:19:38 +0200

    feat: add compress and purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import robotsTxt from "astro-robots-txt";
-
 import partytown from "@astrojs/partytown";
+import compress from "astro-compress";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap({
     i18n: {
-      defaultLocale: "en", 
+      defaultLocale: "en",
       locales: {
         en: "en-GB",
         pl: "pl-PL"
@@ -19,6 +21,6 @@ export default defineConfig({
     }
   }), prefetch({
     intentSelector: ["a[href^='/en']", "a[href^='/pl']"]
-  }), tailwind(), svelte(), robotsTxt(), partytown()],
+  }), tailwind(), svelte(), robotsTxt(), partytown(), purgecss(), compress()],
   site: "https://jpalczewski.github.io"
 });
