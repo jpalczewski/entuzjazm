4420e25187f86661e7ca85ce44b180621bfa6f88
Jacek Palczewski <jpalczewski@gmail.com>
2023-09-06 15:35:27 +0200

    build(pnpm): add prettier plugin and overrides for svelte

--- src/components/MobileMenu.svelte
+++ src/components/MobileMenu.svelte
@@ -1,11 +1,11 @@
 <script>
-    let visible = false;
-    function toggleMenu() {
-      visible = !visible;
-
-    }
+  let visible = false;
+  function toggleMenu() {
+    visible = !visible;
+  }
 </script>
-<div class="btn btn-ghost normal-case text-xl m-1" on:click={toggleMenu} >
+
+<div class="btn btn-ghost normal-case text-xl m-1" on:click={toggleMenu}>
   <svg
     xmlns="http://www.w3.org/2000/svg"
     class="w-6 h-6"
@@ -17,13 +17,14 @@
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="2"
-      d="M4 6h16M4 12h16M4 18h16"></path>
+      d="M4 6h16M4 12h16M4 18h16"
+    ></path>
   </svg>
 </div>
 {#if visible}
-<div id="menu" >
-  <ul class="menu menu-vertical px-1">
-    <slot />
-  </ul>
-</div>
+  <div id="menu">
+    <ul class="menu menu-vertical px-1">
+      <slot />
+    </ul>
+  </div>
 {/if}
--- src/components/ProjectList.svelte
+++ src/components/ProjectList.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
-    import projectSchema from "src/schemas/projectSchema";
-    import type { z } from "astro:content";
-    
-    export let lang: string;
-    type projectType = z.infer<typeof projectSchema>;
-    type projectResponse = Record<string, projectType>;   
+  import projectSchema from "src/schemas/projectSchema";
+  import type { z } from "astro:content";
 
-   
-    async function fetchProjects() {
-            const projectsFetched = await fetch("/api/projects.json");
-            const projects: projectResponse = await projectsFetched.json();
-            return projects;    
-    }
+  export let lang: string;
+  type projectType = z.infer<typeof projectSchema>;
+  type projectResponse = Record<string, projectType>;
 
+  async function fetchProjects() {
+    const projectsFetched = await fetch("/api/projects.json");
+    const projects: projectResponse = await projectsFetched.json();
+    return projects;
+  }
 </script>
 
-{#await fetchProjects() }
-   lolading... 
-{:then projects} 
-
-<div class="overflow-x-auto">
+{#await fetchProjects()}
+  lolading...
+{:then projects}
+  <div class="overflow-x-auto">
     <table class="table table-zebra">
       <!-- head -->
       <thead>
@@ -33,14 +30,13 @@
       </thead>
       <tbody>
         {#each Object.entries(projects) as [_slug, project], index}
-        <tr>
-            <th>{index+1}</th>
+          <tr>
+            <th>{index + 1}</th>
             <td>{project.name[lang]}</td>
             <td>{project.tags.join(" ")}</td>
             <td>{new Date(project.startDate).getFullYear()}</td>
-            <td><button class="btn" >Details</button></td>
-        </tr>
-            
+            <td><button class="btn">Details</button></td>
+          </tr>
         {/each}
       </tbody>
     </table>
